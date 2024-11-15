60e5a03da0dc79199124cd8e3b07f904eb1ac0df
Etienne Rivard <etienne@kerzo.ca>
2024-11-15 15:16:35 -0500

    retroaction

---
+++ correction.md
@@ -0,0 +1,3 @@
+Si tu cliques sur 3 cartes rapidement, les 3 cartes apparaîssent, mais seulement 2 sont retournées après.
+
+Les cartes ne sont pas cachées après une seconde, mais 800 millisecondes.
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,29 +1,43 @@
 import Card from '@mui/material/Card';
-import {ICarte} from '../models/Carte'
+import { ICarte } from '../models/Carte';
 import CardMedia from '@mui/material/CardMedia';
 import CardActionArea from '@mui/material/CardActionArea';
 
 // Composant qui affiche les cartes de chat
-const CardChat = ({ carte, handleCardClick }: { carte: ICarte, handleCardClick: (id: number) => void }) => {
-    return (
-        // je retourne les infos que j'ai besoins sur la carte tournée
-        <Card sx={{ maxWidth: 145, margin: 1 }} onClick={() => !carte.flipped && !carte.matched && handleCardClick(carte.id)}>
-        <CardActionArea>
-            {carte.flipped || carte.matched ? (
-            <CardMedia
-                component="img"
-                height="140"
-                image={carte.image}
-                alt={carte.dessusCarte}
-            />
-            ) : <CardMedia
+// ER : Tu aurais dû faire une interface pour tes props...
+const CardChat = ({
+  carte,
+  handleCardClick,
+}: {
+  carte: ICarte;
+  handleCardClick: (id: number) => void;
+}) => {
+  return (
+    // je retourne les infos que j'ai besoins sur la carte tournée
+    <Card
+      sx={{ maxWidth: 145, margin: 1 }}
+      onClick={() =>
+        !carte.flipped && !carte.matched && handleCardClick(carte.id)
+      }
+    >
+      <CardActionArea>
+        {carte.flipped || carte.matched ? (
+          <CardMedia
+            component="img"
+            height="140"
+            image={carte.image}
+            alt={carte.dessusCarte}
+          />
+        ) : (
+          <CardMedia
             component="img"
             height="140"
             image={carte.imageHidden}
             alt={carte.dessusCarte}
-        />}
-        </CardActionArea>
-        </Card>
-    );
+          />
+        )}
+      </CardActionArea>
+    </Card>
+  );
 };
-export default CardChat
+export default CardChat;
